7a840332ae39f68493aa5662d7350e167f71635f
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-15 20:00:00 +0000

    Feat: 기본 틀 구성

---
+++ src/pages/index.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import { Layout } from "antd";
+import "react-image-gallery/styles/css/image-gallery.css";
+import "antd/dist/antd.css";
+import Gallery from "../components/gallery";
+import Greeting from "../components/greeting";
+import Title from "../components/title";
+import "../styles/index.css";
+
+import GroovePaper from "../assets/GroovePaper.png";
+import Location from "../components/location";
+import CongratulatoryMoney from "../components/congratulatoryMoney";
+import Share from "../components/share";
+import Quote from "../components/quote";
+import Song from "../assets/song.mp4";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// markup
+const { Footer } = Layout;
+
+const IndexPage = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://developers.kakao.com/sdk/js/kakao.min.js";
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.romoveChile(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+    });
+  });
+  return (
+    <>
+      <audio autoPlay loop>
+        <source src={Song} />
+      </audio>
+      <Title />
+      <Greeting />
+      <Gallery />
+      <Location />
+      <Quote />
+      <CongratulatoryMoney />
+      <Share />
+      <Footer
+        style={{
+          background: "#D7CCC8",
+          backgroundImage: `url(${GroovePaper})`,
+          opacity: 0.6,
+          textAlign: "center",
+        }}
+      >
+        Copyright © 2022 Shin Jooyoung
+      </Footer>
+    </>
+  );
+};
+
+export default IndexPage;

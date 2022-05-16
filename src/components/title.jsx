7de8b8e27e4acad45dbacbe584513add8638fabb
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-16 20:00:00 +0000

    Feat: 타이틀 컴포넌트 추가

---
+++ src/components/title.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  WEDDING_DATE,
+  WEDDING_LOCATION,
+  GROOM_NAME,
+  BRIDE_NAME,
+} from "../../config.js";
+import BackgroundVideo from "../assets/BackgroundVideo.mp4";
+import GroovePaper from "../assets/GroovePaper.png";
+
+const Layout = styled.div`
+  background: #efebe9;
+  background-image: url(${GroovePaper});
+  width: 100%;
+  overflow: hidden;
+  margin: 0px auto;
+  position: relative;
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+  padding-top: 42px;
+  font-weight: 500 !important;
+  color: var(--title-color);
+  animation: fadein 3s;
+  -moz-animation: fadein 3s; /* Firefox */
+  -webkit-animation: fadein 3s; /* Safari and Chrome */
+  -o-animation: fadein 3s; /* Opera */
+`;
+
+const VideoBackground = styled.video`
+  background: #efebe9;
+  background-image: url(${GroovePaper});
+  padding-left: 42px;
+  padding-right: 42px;
+  width: 100%;
+`;
+
+const WeddingInvitation = styled.p`
+  font-size: 0.825rem;
+  opacity: 0.45;
+  margin-bottom: 16px;
+`;
+
+const GroomBride = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  opacity: 0.9;
+  margin-bottom: 16px;
+`;
+
+const Schedule = styled.p`
+  font-size: 1.06rem;
+  opacity: 0.65;
+  margin-bottom: 24px;
+`;
+const Title = () => {
+  return (
+    <Layout>
+      <TitleWrapper>
+        <WeddingInvitation>WEDDING INVITATION</WeddingInvitation>
+        <GroomBride>
+          {GROOM_NAME} &#38; {BRIDE_NAME}
+        </GroomBride>
+        <Schedule>
+          {WEDDING_DATE}
+          <br />
+          {WEDDING_LOCATION}
+        </Schedule>
+      </TitleWrapper>
+      <VideoBackground autoPlay loop muted playsInline={true}>
+        <source src={BackgroundVideo} type="video/mp4" />
+      </VideoBackground>
+    </Layout>
+  );
+};
+
+export default Title;

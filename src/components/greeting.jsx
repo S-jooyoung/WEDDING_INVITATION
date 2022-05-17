84b4fb55252662f0da980a529cc82c4db453390e
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-17 20:00:00 +0000

    Feat: 인사 컴포넌트 추가

---
+++ src/components/greeting.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components";
+import { Divider } from "antd";
+import {
+  GROOM_NAME,
+  GROOM_FATHER_NAME,
+  GROOM_MOTHER_NAME,
+  BRIDE_NAME,
+  BRIDE_FATHER_NAME,
+  BRIDE_MOTHER_NAME,
+} from "../../config";
+
+import GroovePaper from "../assets/GroovePaper.png";
+import Flower from "../assets/flower1.png";
+
+const Wrapper = styled.div`
+  background: #efebe9;
+  background-image: url(${GroovePaper});
+  padding-top: 42px;
+  padding-left: 42px;
+  padding-right: 42px;
+  width: 100%;
+`;
+
+const Title = styled.p`
+  font-size: 1rem;
+  color: var(--title-color);
+  font-weight: bold;
+  opacity: 0.85;
+  margin-bottom: 0;
+  text-align: center;
+`;
+
+const Content = styled.p`
+  font-size: 0.875rem;
+  line-height: 1.75;
+  opacity: 0.75;
+  margin-bottom: 16px;
+  width: 100%;
+  text-align: center;
+`;
+
+const GroomBride = styled.p`
+  font-size: 0.875rem;
+  line-height: 1.75;
+  opacity: 0.85;
+  margin-bottom: 0px;
+  width: 100%;
+  text-align: center;
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 1.375rem;
+  padding-bottom: 42px;
+`;
+
+const Greeting = () => {
+  return (
+    <Wrapper>
+      <Divider style={{ marginTop: 32, marginBottom: 32 }} plain>
+        <Title data-aos="fade-up">초대합니다</Title>
+      </Divider>
+      <Image data-aos="fade-up" src={Flower} />
+      <Content data-aos="fade-up">
+        서로 마주 보며 다져온 사랑을
+        <br />
+        <br />
+        이제 함께 한곳을 바라보며 걸어갈 수 있는
+        <br />
+        <br />
+        큰 사랑으로 키우고자 합니다.
+        <br />
+        <br />
+        저희 두 사람이 사랑의 이름으로 지켜나갈 수 있게
+        <br />
+        <br />
+        앞날을 축복해 주시면 감사하겠습니다.
+      </Content>
+      <GroomBride data-aos="fade-up">
+        {GROOM_FATHER_NAME} · {GROOM_MOTHER_NAME}의 장남 {GROOM_NAME}
+        <br />
+        {BRIDE_FATHER_NAME} · {BRIDE_MOTHER_NAME}의 장녀 {BRIDE_NAME}
+      </GroomBride>
+    </Wrapper>
+  );
+};
+
+export default Greeting;

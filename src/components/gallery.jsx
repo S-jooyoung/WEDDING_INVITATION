ccb3702685978cd9848dc0c9d9fb30753b2cc4f8
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-18 20:00:00 +0000

    Feat: 갤러리 컴포넌트 추가

---
+++ src/components/gallery.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import { Divider } from "antd";
+import styled from "styled-components";
+
+import GalleryPhoto1 from "../assets/Gallery_Photo_1.jpg";
+import GalleryPhoto2 from "../assets/Gallery_Photo_2.jpg";
+import GalleryPhoto3 from "../assets/Gallery_Photo_3.jpg";
+import GalleryPhoto4 from "../assets/Gallery_Photo_4.jpg";
+import GalleryPhoto5 from "../assets/Gallery_Photo_5.jpg";
+import GalleryPhoto6 from "../assets/Gallery_Photo_6.jpg";
+import GroovePaper from "../assets/GroovePaper.png";
+
+const Wrapper = styled.div`
+  background: #efebe9;
+  background-image: url(${GroovePaper});
+  padding-top: 42px;
+  padding-left: 42px;
+  padding-right: 42px;
+  width: 100%;
+`;
+
+const Title = styled.p`
+  font-size: 1rem;
+  color: var(--title-color);
+  font-weight: bold;
+  opacity: 0.85;
+  margin-bottom: 0;
+  text-align: center;
+`;
+
+const images = [
+  {
+    original: GalleryPhoto1,
+    thumbnail: GalleryPhoto1,
+  },
+  {
+    original: GalleryPhoto2,
+    thumbnail: GalleryPhoto2,
+  },
+  {
+    original: GalleryPhoto3,
+    thumbnail: GalleryPhoto3,
+  },
+  {
+    original: GalleryPhoto4,
+    thumbnail: GalleryPhoto4,
+  },
+  {
+    original: GalleryPhoto5,
+    thumbnail: GalleryPhoto5,
+  },
+  {
+    original: GalleryPhoto6,
+    thumbnail: GalleryPhoto6,
+  },
+];
+
+const Gallery = () => {
+  return (
+    <Wrapper>
+      <Divider style={{ marginTop: 0, marginBottom: 32 }} plain>
+        <Title>우리의 아름다운 순간</Title>
+      </Divider>
+      <ImageGallery
+        showPlayButton={false}
+        showFullscreenButton={false}
+        items={images}
+      />
+    </Wrapper>
+  );
+};
+
+export default Gallery;

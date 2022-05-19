3059891767bdccff7907198753c2ca454c39b8c8
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-19 20:00:00 +0000

    Feat: 위치 컴포넌트 추가

---
+++ src/components/location.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect } from "react";
+import { Divider } from "antd";
+import styled from "styled-components";
+
+import GroovePaper from "../assets/GroovePaper.png";
+import Flower from "../assets/flower2.png";
+
+const Wrapper = styled.div`
+  background: #efebe9;
+  background-image: url(${GroovePaper});
+  padding-top: 42px;
+  padding-left: 42px;
+  padding-right: 42px;
+  width: 100%;
+`;
+
+const Title = styled.span`
+  font-size: 1rem;
+  color: var(--title-color);
+  font-weight: bold;
+  opacity: 0.85;
+  margin-bottom: 0;
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 1.375rem;
+  padding-bottom: 42px;
+`;
+
+const Content = styled.p`
+  font-size: 0.875rem;
+  line-height: 1.75;
+  opacity: 0.75;
+  width: 100%;
+  text-align: center;
+  padding-top: 42px;
+  padding-bottom: 42px;
+  margin: 0;
+`;
+
+const Map = styled.div`
+  width: 100%;
+  padding: 0;
+`;
+
+const Location = () => {
+  // 카카오 맵 불러오기
+
+  // <!-- 3. 실행 스크립트 -->
+  const executeScript = () => {
+    const scriptTag = document.createElement("script");
+    const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
+    "timestamp" : "1652464367301",
+    "key" : "2a8fe",
+    "mapWidth" : "640",
+    "mapHeight" : "360"
+  }).render();`);
+    scriptTag.appendChild(inlineScript);
+    document.body.appendChild(scriptTag);
+  };
+
+  // <!-- 2. 설치 스크립트 * 지도 퍼가기 서비스를 2개 이상 넣을 경우, 설치 스크립트는 하나만 삽입합니다. -->
+  // document.write 문제가 발생해서 해당 파일을 직접 가져온다음 수정했음
+  const InstallScript = () => {
+    (function () {
+      let c = window.location.protocol === "https:" ? "https:" : "http:";
+      let a = "16137cec";
+
+      if (window.daum && window.daum.roughmap && window.daum.roughmap.cdn) {
+        return;
+      }
+      window.daum = window.daum || {};
+      window.daum.roughmap = {
+        cdn: a,
+        URL_KEY_DATA_LOAD_PRE: c + "//t1.daumcdn.net/roughmap/",
+        url_protocal: c,
+      };
+      let b =
+        c +
+        "//t1.daumcdn.net/kakaomapweb/place/jscss/roughmap/" +
+        a +
+        "/roughmapLander.js";
+
+      // document.write -> doumnet.body.append로 수정
+      const scriptTag = document.createElement("script");
+      scriptTag.src = b;
+      document.body.append(scriptTag);
+      scriptTag.onload = () => {
+        executeScript();
+      };
+    })();
+  };
+
+  useEffect(() => {
+    InstallScript();
+  }, [InstallScript]);
+
+  return (
+    <Wrapper>
+      <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
+        <Title>오시는 길</Title>
+      </Divider>
+      <Image src={Flower} />
+      <Map
+        id="daumRoughmapContainer1652464367301"
+        className="root_daum_roughmap root_daum_roughmap_landing"
+      ></Map>
+      <Content>
+        대구 수성구 두산동 888-2번지
+        <br />
+        호텔수성 수성스퀘어 3층 피오니홀
+        <br />
+        <br />
+        <Title>버스 이용시</Title>
+        <br />
+        <br />
+        410-1, 401 호텔수성 앞 하차
+        <br />
+        수성1-1, 수성3-1, 814 TBC방송국 앞 하차
+        <br />
+        <br />
+        <Title>지하철 이용시</Title>
+        <br />
+        <br />
+        3호선 수성못역 하차 (도보 10분)
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Location;

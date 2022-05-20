4658109aa89a4391d973f6a7e305c9544ee6a2a3
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-20 20:00:00 +0000

    Feat: 인용구 컴포넌트 추가

---
+++ src/components/quote.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+
+import GroovePaper from "../assets/GroovePaper.png";
+import QuotePaper from "../assets/Quote.png";
+import Flower from "../assets/flower1.png";
+
+const Wrapper = styled.div`
+  background: #efebe9;
+  background-image: url(${GroovePaper});
+  padding-top: 42px;
+  padding-left: 42px;
+  padding-right: 42px;
+  width: 100%;
+`;
+
+const Content = styled.span`
+  display: block;
+  margin: 0 auto;
+  font-size: 1.3rem;
+  font-family: "mom_to_daughter";
+  text-align: center;
+  color: var(--title-color);
+  line-height: 2.25rem;
+  opacity: 0.75;
+  background-image: url(${QuotePaper});
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const Image = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 1.375rem;
+  padding-bottom: 42px;
+`;
+
+const Quote = () => {
+  return (
+    <Wrapper>
+      <Image src={Flower} data-aos="fade-up" />
+      <Content data-aos="fade-up">
+        장담하건대. 세상이 다 겨울이어도
+        <br />
+        우리 사랑은 늘봄처럼 따뜻하고
+        <br />
+        간혹, 여름처럼 뜨거울 겁니다
+        <br />
+        <br />
+        - 이수동, (사랑가) -
+        <br />
+        <br />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Quote;

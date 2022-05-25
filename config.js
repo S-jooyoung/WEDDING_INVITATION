e8c290cf2e27660fd7cc440c477d412a6fa25152
S-jooyoung <jooyoung.dev@gmail.com>
2022-05-25 20:26:43 +0900

    Feat:Initial setting

---
+++ config.js
@@ -0,0 +1,21 @@
+export const WEDDING_INVITATION_URL = "http://localhost:8000/";
+export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
+export const KAKAOTALK_SHARE_IMAGE =
+  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+
+export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
+export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+
+export const GROOM_NAME = "○○○";
+export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_FATHER_NAME = "○○○";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_NAME = "○○○";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+
+export const BRIDE_NAME = "○○○";
+export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_FATHER_NAME = "○○○";
+export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_NAME = "○○○";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `weddingInvitation`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
